6896ce908afd481967c809093605478d1e8bdc21
wei.zhou <wei.zhou@edoc2.com>
2018-01-20 16:18:52 +0800

    dos xcopy

---
+++ git/window/dos/backfileXcopy.bat
@@ -0,0 +1,8 @@
+rem 备份当前文件 xcopy
+set DaysAgo=1
+set Today=%date:~0,4%%date:~5,2%%date:~8,2%
+set /a PassDays=%Today%-1
+echo %cd%\%PassDays%
+mkdir %cd%\%Today%\%Today%%time:~0,2%%time:~3,2%%time:~6,2%
+xcopy  %cd%\test  %cd%\%Today%\%Today%%time:~0,2%%time:~3,2%%time:~6,2% /d:%date:~5,2%-%date:~8,2%-%date:~0,4% /s
+pause
