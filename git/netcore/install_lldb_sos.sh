0bc50c6df214464a560b727c300a535d87bfff90
wei.zhou <554961776@qq.com>
2019-11-01 22:41:02 +0800

    netcore 调试安装手册

---
+++ git/netcore/install_lldb_sos.sh
@@ -0,0 +1,13 @@
+
+
+
+dotnet tool install -g dotnet-sos
+dotnet-sos install
+sudo apt-get update
+sudo apt-get install cmake clang-3.9 curl gdb gettext git libicu-dev libssl1.0-dev lldb-3.9 liblldb-3.9-dev libunwind8 llvm-3.9 make python-lldb-3.9 tar wget zip -y
+sudo apt-get install lldb-3.9 llvm-3.9 python-lldb-3.9
+
+
+https://github.com/dotnet/diagnostics/blob/master/documentation/installing-sos-instructions.md
+https://github.com/dotnet/diagnostics/blob/master/documentation/building/linux-instructions.md
+https://github.com/dotnet/diagnostics/blob/master/documentation/lldb/linux-instructions.md
