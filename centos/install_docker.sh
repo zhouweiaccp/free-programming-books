940e0c08e608bc4c7b9f8fd18443137a4d41875f
wei.zhou <554961776@qq.com>
2020-01-09 18:11:50 +0800

    centos docker

--- centos/install_docker.sh
+++ centos/install_docker.sh
@@ -6,11 +6,21 @@
 yum remove docker  docker-common docker-selinux docker-engine
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce-18.03.1.ce
+if [ `arch` == 'aarch64' ]; then
+wget  https://download.docker.com/linux/centos/7/aarch64/stable/Packages/docker-ce-cli-19.03.5-3.el7.aarch64.rpm
+rpm -ivh docker-ce-cli-19.03.5-3.el7.aarch64.rpm
+else
+sudo yum install docker-ce docker-ce-cli containerd.io
+fi
+
+
 systemctl start docker
 #yum erase docker-common-2:1.12.6-68.gitec8512b.el7.centos.x86_64
 systemctl enable docker
 docker version
+
+
+
 
 
 
@@ -19,3 +29,60 @@ sudo groupadd docker     #添加docker用户组
 sudo gpasswd -a $USER docker     #将登陆用户加入到docker用户组中
 newgrp docker     #更新用户组
 docker ps    #测试docker命令是否可以使用sudo正常使用
+
+
+
+#https://www.huaweicloud.com/kunpeng/software/docker.html
+# 这个命令总是会安装最新版本的docker-ce，如果需要安装指定版本的可以参考下面的操作：
+
+# sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
+
+# 方式二：下载软件包安装
+
+# 1)     通过https://download.docker.com/linux/centos/7/aarch64/stable/Packages/，下载指定版本的软件包。
+
+# 2)     执行命令安装软件包及依赖。
+
+
+# “package.rpm”为下载的软件包。
+
+# sudo yum install /path/to/package.rpm
+
+# 3.      启动软件
+
+# 1)     启动Docker。
+
+# sudo systemctl start docker
+
+# 2)     使用一个hello-world镜像验证Docker是否正常。
+
+# sudo docker run hello-world
+
+# 回显内容如下：
+
+# Unable to find image 'hello-world:latest' locally
+# latest: Pulling from library/hello-world
+# 3b4173355427: Pull complete 
+# Digest: sha256:41a65640635299bab090f783209c1e3a3f11934cf7756b09cb2f1e02147c6ed8
+# Status: Downloaded newer image for hello-world:latest
+ 
+# Hello from Docker!
+# This message shows that your installation appears to be working correctly.
+ 
+# To generate this message, Docker took the following steps:
+#  1. The Docker client contacted the Docker daemon.
+#  2. The Docker daemon pulled the "hello-world" image from the Docker Hub.
+#     (arm64v8)
+#  3. The Docker daemon created a new container from that image which runs the
+#     executable that produces the output you are currently reading.
+#  4. The Docker daemon streamed that output to the Docker client, which sent it
+#     to your terminal.
+ 
+# To try something more ambitious, you can run an Ubuntu container with:
+#  $ docker run -it ubuntu bash
+ 
+# Share images, automate workflows, and more with a free Docker ID:
+#  https://hub.docker.com/
+ 
+# For more examples and ideas, visit:
+#  https://docs.docker.com/get-started/
--- centos/install_python3_code.sh
+++ centos/install_python3_code.sh
@@ -5,16 +5,41 @@ yum -y install zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel r
 yum install libffi-devel -y
 #2.下载安装包解压
 cd #回到用户目录
-wget http://npm.taobao.org/mirrors/python/3.7.0/Python-3.7.0.tgz
-tar -Jxvf Python-3.7.0.tgz
+#wget http://npm.taobao.org/mirrors/python/3.7.0/Python-3.7.0.tgz
+#tar -xvf Python-3.7.0.tgz
+#cd Python-3.7.0
+# scrapy 版本问题
+wget https://npm.taobao.org/mirrors/python/3.4.9/Python-3.4.9.tgz
+tar -xvf Python-3.4.9.tgz
 #3.编译安装
-mkdir /usr/local/python3 #创建编译安装目录
-cd Python-3.7.0
+test ! -d /usr/local/python3 && mkdir /usr/local/python3 #创建编译安装目录
+cd Python-3.4.9
 ./configure --prefix=/usr/local/python3
 make && make install
 #4.创建软连接
 ln -s /usr/local/python3/bin/python3 /usr/local/bin/python3
 ln -s /usr/local/python3/bin/pip3 /usr/local/bin/pip3
 
+test ! -d ~/pip && mkdir ~/.pip
+cat > ~/.pip/pip.conf<<efo
+[global]
+index-url = https://mirrors.aliyun.com/pypi/simple
+[install]
+trusted-host = https://pypi.tuna.tsinghua.edu.cn
+efo
+
 pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
 pip3 config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+
+pip3 install scrapy --user
+# pip install --upgrade pip  -i http://pypi.douban.com/simple/
+
+# yum install -y epel-release 
+
+# yum install -y openldap-devel 
+
+# yum install -y python-devel 
+
+# yum install -y python-pip
+
+# pip install --upgrade pip
