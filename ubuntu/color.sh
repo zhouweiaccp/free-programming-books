1710effb0ae9b2f8a54038f950299f410b4e761e
wei.zhou <wei.zhou@qq.com>
2020-01-20 17:55:36 +0800

    linux shell change color

---
+++ ubuntu/color.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "\033[30m black \033[0m"
+echo -e "\033[31m red \033[0m"
+echo -e "\033[32m green \033[0m"
+echo -e "\033[33m yellow \033[0m"
+echo -e "\033[34m blue \033[0m"
+echo -e "\033[35m purple \033[0m"
+echo -e "\033[36m cyan \033[0m"
+echo -e "\033[37m white \033[0m"
